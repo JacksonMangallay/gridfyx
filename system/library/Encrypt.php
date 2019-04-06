66881e30db51848bae56d38f63cbaaa01f6a7139
jackson <jackson.mangallay@hotmail.com>
2019-04-06 16:38:19 +0800

    added RandomString and CryptopRandSecure methods on Encrypt library

--- system/library/Encrypt.php
+++ system/library/Encrypt.php
@@ -53,4 +53,40 @@ class Encrypt{
         return chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
     }
 
+    private function CryptoRandSecure($min, $max){
+
+        $range = $max - $min;
+
+        if($range < 1){
+            return $min;
+        }
+
+        $log = ceil(log($range, 2));
+        $bytes = (int)($log / 8) + 1;
+        $bits = (int)$log + 1;
+        $filter = (int)(1 << $bits) - 1;
+
+        do{
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+            $rnd = $rnd & $filter;
+        }while($rnd > $range);
+
+        return $min + $rnd;
+
+    }
+    
+    public function RandomString($length){
+        $token = "";
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet.= "0123456789";
+        $max = strlen($codeAlphabet);
+    
+        for($i=0; $i < $length; $i++){
+            $token .= $codeAlphabet[$this->CryptoRandSecure(0, $max-1)];
+        }
+    
+        return $token;
+    }
+
 }
