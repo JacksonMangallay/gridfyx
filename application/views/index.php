1b1d95ccff78d271bb49435ff5ec0c7cd9362daf
jackson <jackson.mangallay@hotmail.com>
2019-04-25 16:22:00 +0800

    added welcome message for first time running

--- application/views/index.php
+++ application/views/index.php
@@ -5,11 +5,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gridfyx PHP Framework</title>
+    <title>Gridfyx v1.4</title>
+
+    <style type="text/css">
+        html,body{
+            margin: 0;
+            padding: 0;
+            font-family: Helvetica, Arial, sans-serif;
+            font-size: .9rem;
+            font-weight: normal;
+            color: #4F5155;
+        }
+
+        body{
+            padding: 40px;
+        }
+
+        .wrapper{
+            padding: 20px;
+            border: 1px solid #d0d0d0;
+        }
+
+        h1{
+            margin: 0;
+            font-weight: 100;
+            font-size: 1.7rem;
+        }
+
+        hr{
+            border-bottom: 0;
+            border-left: 0;
+            border-right: 0;
+            border-width: 1px;
+            border-color: #d0d0d0;
+            margin: 20px 0;
+        }
+
+        a{
+            color: #003399;
+        }
+
+        .highlight{
+            display: block;
+            margin-top: 10px;
+            padding: 15px;
+            background-color: #eaeaea;
+        }
+    </style>
+
 </head>
 <body>
-<?php 
-
-?>
+    <div class="wrapper">
+        <h1>Gridfyx 1.4 is now running. Happy coding!</h1>
+        <hr>
+        <p>If this is your first time using Gridfyx, check out the <a href="#!">manual guide here.</a></p>
+        <p>The controller of this page is located at: <span class="highlight">/application/controllers/IndexController.php</span></p>
+        <p>This view file is located at: <span class="highlight">/application/views/index.php</span></p>
+        <p>Thank you for using Gridfyx. Show your support by <a href="https://github.com/JacksonMangallay/gridfyx" target="_blank">giving it a star!</a></p>
+    </div>
 </body>
 </html>
