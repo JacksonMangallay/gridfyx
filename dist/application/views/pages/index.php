50dd7ff7f8d0190685cd3910a8f2a281b90c48d9
jackson <jackson.mangallay@hotmail.com>
2019-09-29 08:46:14 +0800

    updated title

--- dist/application/views/pages/index.php
+++ dist/application/views/pages/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gridfyx v1.4</title>
+    <title>Gridfyx v2.0.0-Beta</title>
     <link rel="shortcut icon" href="<?php url('/favicon.ico'); ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?php url('/test.css'); ?>">
 
