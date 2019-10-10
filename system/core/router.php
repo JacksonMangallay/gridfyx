5d5b56db0ffd69bba4190fbb1f71b765cc0a7c89
jackson <jackson.mangallay@hotmail.com>
2019-10-10 21:25:53 +0800

    defined and throw dedicated exception

--- system/core/router.php
+++ system/core/router.php
@@ -42,7 +42,7 @@ class Router{
     public static function set_default_controller($controller = ''){
 
         if(is_empty($controller)){
-            throw new Exception('Default controller should neither be null nor empty.');
+            throw new UnexpectedValueException();
         }
 
         self::$controller = $controller;
@@ -57,7 +57,7 @@ class Router{
     public static function set_default_method($method = ''){
 
         if(is_empty($method)){
-            throw new Exception('Default method should neither be null nor empty.');
+            throw new UnexpectedValueException();
         }
 
         self::$method = $method;
@@ -72,7 +72,7 @@ class Router{
     public static function set_namespace($namespace = ''){
 
         if(is_empty($namespace)){
-            throw new Exception('Controller namespace is required!');
+            throw new UnexpectedValueException();
         }
 
         self::$namespace = $namespace;
@@ -109,11 +109,11 @@ class Router{
         self::$config->load('routes');
 
         if(!self::has_default_controller()){
-            throw new Exception('Default controller is not set!');
+            throw new UnexpectedValueException();
         }
 
         if(!self::has_default_method()){
-            throw new Exception('Default method is not set!');
+            throw new UnexpectedValueException();
         }
 
         //Fetch all routes depending on request method (GET or POST) 
@@ -264,11 +264,11 @@ class Router{
     private static function store_routes($request_method = 'GET', $url = '', $args = array()){
 
         if(!isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] !== $request_method){
-            throw new Exception('Invalid HTTP request!');
+            throw new UnexpectedValueException();
         }
 
         if(!isset($url) || empty($url)){
-            throw new Exception('URL request is not set!');
+            throw new UnexpectedValueException();
         }
 
         /**
